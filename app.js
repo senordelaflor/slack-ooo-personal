cda638e78301c00dfb4648bccae269e1bec4b92a
Pablo Martinez <senordelaflor@gmail.com>
2019-01-02 19:54:18 -0500

    Make OoO start every day

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-'use strict';
+"use strict";
 
-const logger = require('./lib/logger')();
-const redact = require('redact-object');
-const Bot = require('./lib/bot');
-const Config = require('./lib/config');
-const ON_DEATH = require('death');
+const logger = require("./lib/logger")();
+const redact = require("redact-object");
+const Bot = require("./lib/bot");
+const Config = require("./lib/config");
+const ON_DEATH = require("death");
+const moment = require("moment-timezone");
 
 let bot;
 let config;
 
 // Close the bot gracefully
-ON_DEATH((signal) => {
+ON_DEATH(signal => {
   if (bot instanceof Bot) {
     bot.stop();
   }
 
   // You need to actually kill the process if you overwrite the signal response
   switch (signal) {
-    case 'SIGINT':
-    case 'SIGTERM':
+    case "SIGINT":
+    case "SIGTERM":
       // give the last message a chance to send
       setTimeout(process.exit, 1000);
       break;
@@ -32,9 +33,9 @@ ON_DEATH((signal) => {
 const rawConfig = (() => {
   let retVal;
   try {
-    retVal = require('./config');
+    retVal = require("./config");
   } catch (exception) {
-    retVal = require('./config.default');
+    retVal = require("./config.default");
   }
 
   return retVal;
@@ -43,33 +44,35 @@ const rawConfig = (() => {
 try {
   config = Config.parse(rawConfig);
 } catch (error) {
-  logger.error('Could not parse config', error);
+  logger.error("Could not parse config", error);
   process.exit(1);
 }
 
-logger.info('Using the following configuration:', redact(config, ['token']));
+logger.info("Using the following configuration:", redact(config, ["token"]));
 
-function end () {
-  logger.info('Ending out of office...');
+function end() {
+  logger.info("Ending out of office...");
   if (bot instanceof Bot) {
     bot.stop();
   }
   process.exit();
 }
 
-function start () {
-  logger.info('Starting bot...');
+function start() {
+  logger.info("Starting bot...");
   bot = new Bot(config);
-  setTimeout(() => { bot.start(); }, 1000);
+  setTimeout(() => {
+    bot.start();
+  }, 1000);
 
   // Set a clock to turn off
   const rightnow = Date.now();
   if (config.app.timebox.end > rightnow) {
     const endDate = new Date(config.app.timebox.end);
-    const diffMS = (config.app.timebox.end - rightnow);
+    const diffMS = config.app.timebox.end - rightnow;
     // Check if diff is greater than MAX_INT32
     // This causes the setTimeout to overflow and launch immediataly
-    if (diffMS > 0x7FFFFFFF) {
+    if (diffMS > 0x7fffffff) {
       logger.error(`
         32 Int overflow error!
         ${endDate} is too far into the future.
@@ -83,16 +86,28 @@ function start () {
 }
 
 // Check if then end is later than now or not set (no end in sight)
-const now = Date.now();
-if (config.app.timebox.end > now || !config.app.timebox.end) {
+const now = moment().tz(config.app.timebox.timezone);
+const dayOfTheWeek = moment()
+  .tz(config.app.timebox.timezone)
+  .day();
+const isWeekendDay = global.dayOfTheWeek === 0 || global.dayOfTheWeek === 6;
+
+const startOfOOO = moment()
+  .tz(config.app.timebox.timezone)
+  .hours(config.app.timebox.start)
+  .minutes(0); // 5 PM
+const endOfOOO = moment()
+  .tz(config.app.timebox.timezone)
+  .hours(config.app.timebox.end)
+  .minutes(0);
+if (!isWeekendDay) {
   // Check if we are past the start time
-  if (config.app.timebox.start < now) {
+  if (now.isAfter(startOfOOO) || now.isBefore(endOfOOO)) {
     start();
   } else {
-    const startDate = new Date(config.app.timebox.start);
-    logger.info(`Waiting until ${startDate} to start bot...`);
+    logger.info(`Waiting until ${startOfOOO}th hour to start bot...`);
     setTimeout(start, config.app.timebox.start - now);
   }
 } else {
-  logger.error(`Cannot start with historical end time: ${new Date(config.app.timebox.end)}`);
+  logger.error(`It is the weekend`);
 }
